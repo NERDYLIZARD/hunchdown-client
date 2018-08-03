b2f8c5b85ca065ce0afa3bb38311379557692e08
NERDYLIZARD <bouasavanhhop@gmail.com>
2018-08-03 16:03:27 +0700

    test boxSaga

---
+++ src/modules/boxes/saga.spec.js
@@ -0,0 +1,84 @@
+/**
+ * Created on 03-Aug-18.
+ */
+import _ from 'lodash';
+import faker from 'faker';
+import { call, put } from 'redux-saga/effects';
+import { cloneableGenerator } from 'redux-saga/utils';
+import Services from './services';
+import { generateBox } from '../../utils/test/mockDataFactory';
+import { createBox, deleteBox, loadBoxes } from './saga';
+import {
+  createBoxSuccess,
+  deleteBoxSuccess,
+  loadBoxesSuccess,
+} from './actions';
+
+
+describe('Box Sagas', () => {
+  /**
+   * Load Boxes
+   */
+  describe('loadBoxes', () => {
+    const generator = cloneableGenerator(loadBoxes)();
+
+    it('should call api to fetch `boxes`', () => {
+      expect(generator.next().value).toEqual(call(Services.find));
+    });
+
+    it('should dispatch `loadBoxesSuccess()` with the fetched `boxes` as its argument', () => {
+      const clone = generator.clone();
+      const boxes = [generateBox(), generateBox()];
+      expect(clone.next(boxes).value).toEqual(put(loadBoxesSuccess(boxes)));
+      expect(clone.next().done).toBe(true);
+    });
+    // it('should call api to fetch boxes and dispatch loadBoxesSuccess with the fetched boxes', () => {
+    //   const clone = generator.clone;
+    //   const error = { message: 'later' };
+    // expect(clone.throw(error).value).toEqual(put(loadBoxesFailed(box)));
+    // expect(clone.next().done).toBe(true);
+    // });
+  });
+
+
+  /**
+   * Create Box
+   */
+  describe('createBox', () => {
+    const action = {box: _.omit(generateBox(), 'id')};
+    const generator = cloneableGenerator(createBox)(action);
+
+    it('should call api to create a `box` with a `box` object as its argument', () => {
+      expect(generator.next().value).toEqual(call(Services.create, action.box));
+    });
+    it('should dispatch `createBoxSuccess()` with the created `box` as its argument', () => {
+      const clone = generator.clone();
+      const box = {
+        ...action.box,
+        id: faker.random.uuid(),
+      };
+      expect(clone.next(box).value).toEqual(put(createBoxSuccess(box)));
+      expect(clone.next().done).toBe(true);
+    });
+    // error case (later)
+  });
+
+
+  /**
+   * Delete Box
+   */
+  describe('deleteBox', () => {
+    const action = {box: generateBox()};
+    const generator = cloneableGenerator(deleteBox)(action);
+    it('should call api to delete a `box` with a `box` object as its argument', () => {
+      expect(generator.next().value).toEqual(call(Services.delete, action.box));
+    });
+    it('should dispatch `deleteBoxSuccess()` with the box that has been passed in `deleteBox()` as it argument', () => {
+      const clone = generator.clone();
+      expect(clone.next().value).toEqual(put(deleteBoxSuccess(action.box)));
+      expect(clone.next().done).toBe(true);
+    });
+    // error case (later)
+  });
+
+});
--- src/modules/hunches/saga.spec.js
+++ src/modules/hunches/saga.spec.js
@@ -25,11 +25,11 @@ describe('Hunch Sagas', () => {
   describe('loadHunches', () => {
     const generator = cloneableGenerator(loadHunches)();
 
-    it('should call api to fetch hunches', () => {
+    it('should call api to fetch `hunches`', () => {
       expect(generator.next().value).toEqual(call(Services.find));
     });
 
-    it('should dispatch loadHunchesSuccess() with the fetched hunches as its argument', () => {
+    it('should dispatch `loadHunchesSuccess()` with the fetched `hunches` as its argument', () => {
       const clone = generator.clone();
       const hunches = [generateHunch(), generateHunch()];
       expect(clone.next(hunches).value).toEqual(put(loadHunchesSuccess(hunches)));
@@ -51,10 +51,10 @@ describe('Hunch Sagas', () => {
     const action = {id: faker.random.uuid()};
     const generator = cloneableGenerator(loadHunch)(action);
 
-    it('should call api to fetch a hunch by id', () => {
+    it('should call api to fetch a `hunch` by id', () => {
       expect(generator.next().value).toEqual(call(Services.get, action.id));
     });
-    it('should dispatch loadHunchSuccess() with the fetched hunch as its argument', () => {
+    it('should dispatch `loadHunchSuccess()` with the fetched `hunch` as its argument', () => {
       const clone = generator.clone();
       const hunch = generateHunch();
       expect(clone.next(hunch).value).toEqual(put(loadHunchSuccess(hunch)));
@@ -71,10 +71,10 @@ describe('Hunch Sagas', () => {
     const action = {hunch: _.omit(generateHunch(), 'id')};
     const generator = cloneableGenerator(createHunch)(action);
 
-    it('should call api to create a hunch with a hunch object as its argument', () => {
+    it('should call api to create a `hunch` with a `hunch` object as its argument', () => {
       expect(generator.next().value).toEqual(call(Services.create, action.hunch));
     });
-    it('should dispatch createHunchSuccess() with the created hunch as its argument', () => {
+    it('should dispatch `createHunchSuccess()` with the created `hunch` as its argument', () => {
       const clone = generator.clone();
       const hunch = {
         ...action.hunch,
@@ -94,10 +94,10 @@ describe('Hunch Sagas', () => {
     const action = {hunch: generateHunch()};
     const generator = cloneableGenerator(updateHunch)(action);
 
-    it('should call api to update a hunch with a hunch object as its argument', () => {
+    it('should call api to update a `hunch` with a `hunch` object as its argument', () => {
       expect(generator.next().value).toEqual(call(Services.update, action.hunch));
     });
-    it('should dispatch updateHunchSuccess() with the updated hunch as its argument', () => {
+    it('should dispatch `updateHunchSuccess()` with the updated `hunch` as its argument', () => {
       const clone = generator.clone();
       const hunch = action.hunch;
       expect(clone.next(hunch).value).toEqual(put(updateHunchSuccess(hunch)));
@@ -113,10 +113,10 @@ describe('Hunch Sagas', () => {
   describe('deleteHunch', () => {
     const action = {hunch: generateHunch()};
     const generator = cloneableGenerator(deleteHunch)(action);
-    it('should call api to delete a hunch with a hunch object as its argument', () => {
+    it('should call api to delete a `hunch` with a `hunch` object as its argument', () => {
       expect(generator.next().value).toEqual(call(Services.delete, action.hunch));
     });
-    it('should dispatch deleteHunchSuccess() with the hunch that has been passed in deleteHunch() as it argument', () => {
+    it('should dispatch `deleteHunchSuccess()` with the hunch that has been passed in `deleteHunch()` as it argument', () => {
       const clone = generator.clone();
       expect(clone.next().value).toEqual(put(deleteHunchSuccess(action.hunch)));
       expect(clone.next().done).toBe(true);
