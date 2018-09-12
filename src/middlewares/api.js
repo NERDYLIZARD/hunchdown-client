4ade969b965b478ec5dfe6e2c4fc5050702d3180
NERDYLIZARD <bouasavanhhop@gmail.com>
2018-09-12 11:15:38 +0700

    test `apiMiddleware`: `callApi` function

--- src/middlewares/api.js
+++ src/middlewares/api.js
@@ -22,15 +22,15 @@ export const getNextPageUrl = response => {
 
 
 // json server
-const API_ROOT = 'http://localhost:3002';
+export const API_ROOT = 'http://localhost:3002';
 
 // nodejs dev server
-// const API_ROOT = 'http://localhost:4000/api';
+// export const API_ROOT = 'http://localhost:4000/api';
 
 
 // Fetches an API response and normalizes the result JSON according to schema.
 // This makes every API response have the same shape, regardless of how nested it was.
-export const callApi = (endpoint, schema, method, data) => {
+export const callApi = (endpoint, schema, method, data = {}) => {
   const url = (endpoint.indexOf(API_ROOT) === -1) ? API_ROOT + endpoint : endpoint;
 
   return axios({
@@ -38,30 +38,31 @@ export const callApi = (endpoint, schema, method, data) => {
     method,
     // for deletion, we don't want to send data, but we still need data to return as action's payload.
     data: method === 'DELETE' ? null : data
-  }).then(response => {
-    // in case server return empty of object, i.e. status 204, normalize data that passed in
-    const toBeNormalized = isEmpty(response.data) ? data : response.data;
-    const {entities, result} = normalize(toBeNormalized, schema);
-    const json = {
-      entities,
-      result
-    };
-    const nextPageUrl = getNextPageUrl(response);
-    if (nextPageUrl) {
-      json.nextPageUrl = nextPageUrl;
-    }
-    // other header fields
-    return json;
-
-  }).catch(error => {
-    if (error.response) {
-      return Promise.reject(error.response.data);
-    } else if (error.request) {
-      return Promise.reject({message: 'The request was made but no response was received'});
-    } else {
-      return Promise.reject(error);
-    }
   })
+    .then(response => {
+      // in case server return empty of object, i.e. status 204, normalize data that passed in
+      const toBeNormalized = isEmpty(response.data) ? data : response.data;
+      const {entities, result} = normalize(toBeNormalized, schema);
+      const json = {
+        entities,
+        result
+      };
+      const nextPageUrl = getNextPageUrl(response);
+      if (nextPageUrl) {
+        json.nextPageUrl = nextPageUrl;
+      }
+      // other header fields
+      return json;
+
+    }).catch(error => {
+      if (error.response) {
+        return Promise.reject(error.response.data);
+      } else if (error.request) {
+        return Promise.reject({message: 'The request was made but no response was received'});
+      } else {
+        return Promise.reject(error);
+      }
+    })
 
 };
 
--- src/middlewares/api.spec.js
+++ src/middlewares/api.spec.js
@@ -1,36 +1,166 @@
 /**
  * Created on 11-Sep-18.
  */
-import { getNextPageUrl } from './api';
+import * as apiMiddleware from './api';
+import { boxSchema as schema } from '../normalizr-schema';
+import { normalize } from 'normalizr';
+import axios from 'axios';
 
-describe('getNextPageUrl()', () => {
+jest.mock('axios');
+const {API_ROOT, callApi, getNextPageUrl} = apiMiddleware;
 
-  describe('when `response` argument does not have `link` property', () => {
-    it('returns null', () => {
-      const response = {headers: {link: undefined}};
-      expect(getNextPageUrl(response)).toBe(null);
-    });
-  });
+describe('Api Middleware', () => {
 
-  describe('when `response` argument has `link` property', () => {
-    describe('when `link` string does not contain `next` attribute', () => {
+  describe('getNextPageUrl()', () => {
+    describe('when `response` argument does not have `link` property', () => {
       it('returns null', () => {
-        const response = {headers: {link: 'www.linkdoesnotcontainnext.com'}};
+        const response = {headers: {link: undefined}};
         expect(getNextPageUrl(response)).toBe(null);
       });
     });
 
-    describe('when `link` string contains `next` attribute', () => {
-      it('extracts `next` link', () => {
-        const response = {
-          headers: {
-            link: '<https://api.github.com/user/repos?page=3&per_page=100>; rel="next",\n' +
-            '  <https://api.github.com/user/repos?page=50&per_page=100>; rel="last"'
-          }
-        };
-        expect(getNextPageUrl(response)).toBe('https://api.github.com/user/repos?page=3&per_page=100');
+    describe('when `response` argument has `link` property', () => {
+      describe('when `link` string does not contain `next` attribute', () => {
+        it('returns null', () => {
+          const response = {headers: {link: 'www.linkdoesnotcontainnext.com'}};
+          expect(getNextPageUrl(response)).toBe(null);
+        });
+      });
+
+      describe('when `link` string contains `next` attribute', () => {
+        it('extracts `next` link', () => {
+          const response = {
+            headers: {
+              link: '<https://api.github.com/user/repos?page=3&per_page=100>; rel="next",\n' +
+              '  <https://api.github.com/user/repos?page=50&per_page=100>; rel="last"'
+            }
+          };
+          expect(getNextPageUrl(response)).toBe('https://api.github.com/user/repos?page=3&per_page=100');
+        });
       });
     });
+  });
+
+  describe('callApi()', () => {
+    const endpoint = '/endpoint';
+    const method = 'GET';
+
+    beforeEach(() => {
+      axios.mockRestore();
+      // default resolve
+      axios.mockImplementation(() => Promise.resolve());
+    });
+
+    it('appends `endpoint` to `API_ROOT` before passing as `url` to `axios`', () => {
+      const method = 'GET';
+      callApi(endpoint, null, method);
+      expect(axios).toBeCalledWith({url: API_ROOT + endpoint, method, data: {}});
+    });
+
+    describe('calls on `DELETE` method', () => {
+      it('calls `axios` with `data = null`', () => {
+        const method = 'DELETE';
+        callApi(endpoint, null, method);
+        expect(axios).toBeCalledWith({url: API_ROOT + endpoint, method, data: null});
+      });
+    });
+
+    describe('does not call on `DELETE` method', () => {
+      it('calls `axios` with `data = data`', () => {
+        const method = 'POST';
+        const data = {foo: 'bar'};
+        callApi(endpoint, null, method, data);
+        expect(axios).toBeCalledWith({url: API_ROOT + endpoint, method, data});
+      });
+    });
+
+    describe('when server response', () => {
+      describe('is an empty object', () => {
+        it('return normalized argument `data`', () => {
+          axios.mockImplementation(() => Promise.resolve({}));
+          const method = 'POST';
+          const data = {foo: 'bar'};
+          callApi(endpoint, schema, method, data)
+            .then(response => {
+              expect(response).toEqual(normalize(data, schema));
+            });
+        });
+      });
+
+      describe('is not an empty object', () => {
+        it('return normalized `server response`', () => {
+          const serverResponse = {zoo: 'zaa'};
+          axios.mockImplementation(() => Promise.resolve(serverResponse));
+          const method = 'POST';
+          const data = {foo: 'bar'};
+          callApi(endpoint, schema, method, data)
+            .then(response => {
+              expect(response).toEqual(normalize(serverResponse, schema));
+            });
+        });
+      });
+
+      describe('contains `link` of `nextPageUrl`', () => {
+        it('returns `nextPageUrl` in json response', () => {
+          const getNextPageUrl = jest.spyOn(apiMiddleware, 'getNextPageUrl');
+          getNextPageUrl.mockReturnValue('https://api.github.com/user/repos?page=3&per_page=100');
+          const nextPageUrl = getNextPageUrl();
+
+          const method = 'POST';
+          callApi(endpoint, schema, method)
+            .then(response => {
+              expect(response.nextPageUrl).toBe(nextPageUrl);
+            });
+        });
+      });
+    });
+
+    describe('sever response error', () => {
+      beforeEach(() => {
+        axios.mockRestore();
+        // default reject
+        axios.mockImplementation(() => Promise.reject());
+      });
+
+      describe('is `response` error', () => {
+        it('return `Promise.reject(error.response.data)`', () => {
+          const serverError = {response: {data: 'error'}};
+          axios.mockImplementation(() => Promise.reject(serverError));
+          callApi(endpoint, schema, method)
+            .catch(error => {
+              expect(error).toEqual(serverError.response.data);
+            });
+        });
+      });
+
+      describe('is `request` error', () => {
+        it('return `Promise.reject({message})`', () => {
+          const serverError = {request: {}};
+          axios.mockImplementation(() => Promise.reject(serverError));
+          callApi(endpoint, schema, method)
+            .catch(error => {
+              expect(error).toEqual({message: 'The request was made but no response was received'});
+            });
+        });
+      });
+
+      describe('is general error', () => {
+        it('return `Promise.reject(error)`', () => {
+          const serverError = {};
+          axios.mockImplementation(() => Promise.reject(serverError));
+          callApi(endpoint, schema, method)
+            .catch(error => {
+              expect(error).toEqual(serverError);
+            });
+        });
+      });
+    });
+
+  });
+
+
+  describe('apiMiddleware', () => {
+
   });
 
 });
