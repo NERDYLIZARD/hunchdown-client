b14d6718dd842bbd5811f64f8cf146f12d6a970b
Hoppy Bouasavanh <bouasavanhhop@gmail.com>
2018-10-20 16:57:30 +0700

    refactor classname appending with `classnames` package

--- src/components/common/Grid.js
+++ src/components/common/Grid.js
@@ -3,14 +3,11 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 const Grid = ({items, render, className}) => {
-  let _className = 'grid';
-  if (className)
-    _className = `${_className} ${className}`;
-
   return (
-    <div className={_className}>
+    <div className={classnames('grid', className)}>
       <div className="row">
         {items.map(item =>
           <div className="grid__item col-sm-12 col-md-6 col-lg-4 col-xl-3" key={item.id}>
